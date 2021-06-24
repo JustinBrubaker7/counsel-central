cbaacfdce617da316f0c4e03e06d803328a16cb7
Justin Brubaker <justinbrubaker7@gmail.com>
2021-06-23 21:30:46 -0700

    starting router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 import './index.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
@@ -10,8 +16,37 @@ function App() {
   return (
     <div className="App">
     <h1>Counsel Central</h1>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>  
   );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
 }
 
 export default App;
