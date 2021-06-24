c9170891daf7d93ed4087ede1ee163ec2fe31c0b
Justin brubaker <justinbrubaker7@gmail.com>
2021-06-24 12:19:10 -0700

    layout of external landing page done

--- client/src/components/HeaderExternal/HeaderExternal.js
+++ client/src/components/HeaderExternal/HeaderExternal.js
@@ -2,6 +2,7 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import ExternalNav from '../ExternalNav'
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -61,115 +62,18 @@ export default function HeaderExternal() {
       </div>
 
       <div className="relative pt-6 pb-16 sm:pb-24">
-        <Popover>
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                <nav
-                  className="relative flex items-center justify-between sm:h-10 md:justify-center"
-                  aria-label="Global"
-                >
-                  <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                    <div className="flex items-center justify-between w-full md:w-auto">
-                      <a href="#">
-                        <span className="sr-only">Workflow</span>
-                        <img
-                          className="h-8 w-auto sm:h-10"
-                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                          alt=""
-                        />
-                      </a>
-                      <div className="-mr-2 flex items-center md:hidden">
-                        <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                          <span className="sr-only">Open main menu</span>
-                          <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                        </Popover.Button>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="hidden md:flex md:space-x-10">
-                    {navigation.map((item) => (
-                      <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                  <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
-                    <span className="inline-flex rounded-md shadow">
-                      <a
-                        href="/"
-                        className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
-                      >
-                        Log in
-                      </a>
-                    </span>
-                  </div>
-                </nav>
-              </div>
-
-              <Transition
-                show={open}
-                as={Fragment}
-                enter="duration-150 ease-out"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="duration-100 ease-in"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Popover.Panel
-                  focus
-                  static
-                  className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-                >
-                  <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div className="px-5 pt-4 flex items-center justify-between">
-                      <div>
-                        <img
-                          className="h-8 w-auto"
-                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                          alt=""
-                        />
-                      </div>
-                      <div className="-mr-2">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                          <span className="sr-only">Close menu</span>
-                          <XIcon className="h-6 w-6" aria-hidden="true" />
-                        </Popover.Button>
-                      </div>
-                    </div>
-                    <div className="px-2 pt-2 pb-3">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                    <a
-                      href="/"
-                      className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                    >
-                      Log in
-                    </a>
-                  </div>
-                </Popover.Panel>
-              </Transition>
-            </>
-          )}
-        </Popover>
-
-        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+    
+    
+    <ExternalNav />
+    
+            <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span className="block xl:inline">Counseling software</span>{' '}
               <span className="block text-indigo-600 xl:inline"> that isn't scary</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              Councel Central has all your needs in mind with cool features that are easy to use and a simple interface. We have your rehabilitation center in mind from the start. Built
+              Counsel Central has all your needs in mind with cool features that are easy to use and a simple interface. We have your rehabilitation center in mind from the start. Built
               tailor-made for you in mind. Try your free demo today
             </p>
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
--- client/src/pages/HomeExternal.js
+++ client/src/pages/HomeExternal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HeaderExternal from '../components/HeaderExternal/HeaderExternal'
 import FeatureSectionExternal from '../components/FeatureSectionExternal'
 import TeamExternal from '../components/TeamExternal/TeamExternal'
+import Pricing from '../components/Pricing/Pricing'
 import Footer from '../components/FooterExternal/Footer'
 import CallToAction from '../components/CallToAction/CallToAction'
 
@@ -11,6 +12,7 @@ const HomeExternal = () => {
            <HeaderExternal />
            <FeatureSectionExternal />
            <CallToAction />
+           <Pricing />
            <TeamExternal />
            <Footer />
         </div>
