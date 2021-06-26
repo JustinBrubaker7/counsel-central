19e2358eebf7883814818ac0976f1be56d3b8941
Justin Brubaker <justinbrubaker7@gmail.com>
2021-06-26 10:23:15 -0700

    unsure changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import AppNav from './components/AppNav'
 import ExternalNav from './components/ExternalNav'
 import HomeExternal from './pages/HomeExternal';
 import LoginForm from './components/LoginForm/LoginForm';
+import Pricing from './components/Pricing/Pricing';
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -19,15 +20,19 @@ function App() {
   return (
     <>
     <Router>
+    <Switch>
     <Route exact path="/">
-    {!loggedIn ? <HomeExternal /> : <AppNav />}
+      {!loggedIn ? <HomeExternal /> : <AppNav />}
     </Route>
-
-    
     <Route exact path="/login">
-     <ExternalNav />
-     <LoginForm />
+      <ExternalNav />
+      <LoginForm />
     </Route>
+    <Route exact path="/pricing">
+      <ExternalNav />
+      <Pricing />
+    </Route>
+    </Switch>
     
     </Router>
     </>  
