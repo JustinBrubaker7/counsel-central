fa6bec76d1781feb06281b4dd830c7b5e3ff2d7c
Daniel Moore <wron2@hotmail.com>
2021-06-28 19:52:22 -0700

    Changed the naming conventions of Counselor and Admin routes

--- routes/counselorRoutes.js
+++ routes/counselorRoutes.js
@@ -7,37 +7,15 @@ const transporter = require("../config/nodemailer");
 
 // Routes all defined on /api/admin
 
-// Creates a new Resident /api/counselor/create
+// Creates a new counselor /api/admin/create
 router.post("/create", async (req, res) => {
   try {
-    await Resident.create({
+    await Counselor.create({
       // Added the center key which needs to be passed through the request, along with the rest of the information
-      center_id: req.body.centerID,
-      counselor_id: req.body.counselorID,
-      resident_firstName: req.body.resident_firstName,
-      resident_lastName: req.body.resident_lastName,
-      age: req.body.age,
-      DOB: req.body.DOB,
-      phoneNumber: req.body.phoneNumber,
-      address: req.body.address,
-      city: req.body.city,
-      state: req.body.state,
-      zipCode: req.body.zipCode,
-      allergies: req.body.allergies,
-      gender: req.body.gender,
-      disabled: req.body.disabled,
-      disabled_explanation: req.body.disabled_explanation,
-      emergencyContact_firstName: req.body.emergencyContact_firstName,
-      emergencyContact_lastName: req.body.emergencyContact_lastName,
-      contactRelationship: req.body.contactRelationship,
-      relationship_address: req.body.relationship_address,
-      relationship_city: req.body.relationship_city,
-      relationship_state: req.body.relationship_state,
-      relationship_zipCode: req.body.relationship_zipCode,
-      drug_choice: req.body.drug_choice,
-      years_used: req.body.years_used,
-      sobriety_length: req.body.sobriety_length,
-      hasDetoxed: req.body.hasDetoxed,
+      center_id: req.body.centerKey,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
     }).then((data) => {
       res.json(data);
     });
@@ -46,10 +24,10 @@ router.post("/create", async (req, res) => {
   }
 });
 
-// Removes a Resident /api/counselor/remove
+// Removes a counselor /api/admin/remove
 router.delete("/remove", async (req, res) => {
   try {
-    await Resident.destroy({
+    await Counselor.destroy({
       where: {
         id: req.body.id,
       },
@@ -61,42 +39,20 @@ router.delete("/remove", async (req, res) => {
   }
 });
 
-// Updates a Resident /api/counselor/update
+// Updates a couselor /api/admin/update
 router.put("/update", async (req, res) => {
   try {
-    await Resident.update(
+    await Counselor.update(
       {
         where: {
           id: req.body.id,
         },
       },
       {
-        center_id: req.body.centerID,
-        counselor_id: req.body.counselorID,
-        resident_firstName: req.body.resident_firstName,
-        resident_lastName: req.body.resident_lastName,
-        age: req.body.age,
-        DOB: req.body.DOB,
-        phoneNumber: req.body.phoneNumber,
-        address: req.body.address,
-        city: req.body.city,
-        state: req.body.state,
-        zipCode: req.body.zipCode,
-        allergies: req.body.allergies,
-        gender: req.body.gender,
-        disabled: req.body.disabled,
-        disabled_explanation: req.body.disabled_explanation,
-        emergencyContact_firstName: req.body.emergencyContact_firstName,
-        emergencyContact_lastName: req.body.emergencyContact_lastName,
-        contactRelationship: req.body.contactRelationship,
-        relationship_address: req.body.relationship_address,
-        relationship_city: req.body.relationship_city,
-        relationship_state: req.body.relationship_state,
-        relationship_zipCode: req.body.relationship_zipCode,
-        drug_choice: req.body.drug_choice,
-        years_used: req.body.years_used,
-        sobriety_length: req.body.sobriety_length,
-        hasDetoxed: req.body.hasDetoxed,
+        center_id: req.body.center_id,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       }
     ).then((data) => {
       res.json(data);
