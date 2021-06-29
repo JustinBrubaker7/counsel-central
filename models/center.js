849f05dbd059cbd4295bc33464e35d3f74647ff2
Daniel Moore <wron2@hotmail.com>
2021-06-28 20:44:40 -0700

    Fixed model syntax a bit, added password hashing

--- models/center.js
+++ models/center.js
@@ -1,90 +1,100 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection"); 
+const sequelize = require("../config/connection");
 
 class Center extends Model {}
 
 Center.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        address: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        city: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        state: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        zipCode: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        residentCount: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                isNumeric: true,
-            },
-        bedCount: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        counselorCount: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        director_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isEmail: true,
-            },
-        },
-        phone: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                isNumeric: true,
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: [8],
-            },
-        },
-
-        },
-    }
-)
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zipCode: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    residentCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    bedCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    counselorCount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    director_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "center",
+  }
+);
 
 //center name
 //center address => city ,state, zip
 //max number of beds
-//name of point of contact, email, and phone number => 
+//name of point of contact, email, and phone number =>
 //password
 //current number of residents
 // number of counsellors
 // website
 //
-class Center extends Model {}
 
 module.exports = Center;
--- models/counselor.js
+++ models/counselor.js
@@ -4,40 +4,45 @@ const sequelize = require("../config/connection");
 class Counselor extends Model {}
 
 Counselor.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            validate: {
-                isEmail: true,
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: [8],
-            },
-        },
-
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
 
-    {
-        sequelize,
-        freezeTableNAme: true,
-        underscored: true,
-        modelName: 'counselor',
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "center",
+  }
 );
 
 //name
