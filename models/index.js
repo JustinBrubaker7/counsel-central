8763ac33e6824dafea40d5dc4b16ef95eabc2172
Daniel Moore <wron2@hotmail.com>
2021-06-30 17:40:16 -0700

    Changed up the res.send on some routes for responses

--- models/index.js
+++ models/index.js
@@ -3,19 +3,18 @@ const Center = require("./center");
 const Resident = require("./resident");
 
 Center.hasMany(Counselor, {
-    foreignKey: 'counselor_id,'
+  foreignKey: "counselor_id,",
 });
 Counselor.belongsTo(Center, {
-    foreignKey: 'center_id',
+  foreignKey: "center_id",
 });
 
 Counselor.hasMany(Resident, {
-    foreignKey: 'resident_id',
+  foreignKey: "resident_id",
 });
 
 Resident.belongsTo(Counselor, {
-   foreignKey: 'counselor_id', 
+  foreignKey: "counselor_id",
 });
-
 
 module.exports = { Counselor, Center, Resident };
