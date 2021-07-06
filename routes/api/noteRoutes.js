e5e56e9f13c9bc8f2c42776d920e0d1795f76b12
Daniel Moore <wron2@hotmail.com>
2021-07-06 15:41:55 -0700

    Updated the add/remove/update routes for Notes

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,44 +1,45 @@
 const router = require("express").Router();
-const { Center, Counselor, Resident } = require("../../models");
+const { Center, Counselor, Resident, Note } = require("../../models");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 //const transporter = require("../config/nodemailer");
 
-// ---------- Uncomment When NOTE Model has been Added --------------
+// Create a new note /api/note/create
+router.post("/create", async (req, res) => {
+  newNote = await Note.create({
+    title: req.params.title,
+    body: req.params.body,
+    counselor_id: req.body.counselor_id,
+    resident_id: req.body.resident_id,
+  });
+});
 
-// Routes all defined on /api/note
+// Create a new note /api/note/create
+router.post("/update", async (req, res) => {
+  newNote = await Note.update(
+    {
+      where: {
+        id: req.params.id,
+      },
+    },
+    {
+      title: req.params.title,
+      body: req.params.body,
+      side_note: req.params.side_note,
+      counselor_id: req.params.counselor_id,
+      resident_id: req.params.resident_id,
+    }
+  );
+});
 
-// // Create a new note /api/note/create
-// router.post("/create", async (req, res) => {
-//   newNote = await Note.create({
-//     counselor_id: req.body.counselor_id,
-//     resident_id: req.body.resident_id,
-//     body: req.params.body,
-//   });
-// });
-
-// // Create a new note /api/note/create
-// router.post("/update", async (req, res) => {
-//   newNote = await Note.update(
-//     {
-//       where: {
-//         id: req.params.id,
-//       },
-//     },
-//     {
-//       body: req.params.body,
-//     }
-//   );
-// });
-
-// // Create a new note /api/note/create
-// router.delete("/delete", async (req, res) => {
-//   newNote = await Note.destroy({
-//     where: {
-//       id: req.params.id,
-//     },
-//   });
-// });
+// Create a new note /api/note/create
+router.delete("/delete", async (req, res) => {
+  newNote = await Note.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+});
 
 module.exports = router;
