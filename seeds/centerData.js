86b5a058449094ad72d6a877a65c30730554911f
Justin Brubaker <justinbrubaker7@gmail.com>
2021-07-07 19:44:09 -0700

    making changes

--- seeds/centerData.js
+++ seeds/centerData.js
@@ -16,40 +16,21 @@ const centerData = [
     password: "IThinkThisIsAPassword",
   },
 
-    {
-        name: 'Riverside Rehabilitation',
-        address: '3000 Big Springs Roada',
-        city: 'Riverside',
-        state: 'Califorina',
-        zipCode: '92510',
-        residentCount: 5,
-        bedCount: 5,
-        counselorCount: 5,
-        director_name: 'Tom Holland',
-        email: 'tholland@riverside_rehab.org',
-        phone: 5126654350,
-    },
-
-];
-
-const seedCenter = () => Center.bulkCreate(centerData);
-
-module.exports = seedCenter;
-
   {
-    name: "Riverside Rehabilitation",
-    address: "3000 Big Springs Roada",
-    city: "Riverside",
-    state: "Califorina",
-    zipCode: "92510",
+    name: 'Riverside Rehabilitation',
+    address: '3000 Big Springs Roada',
+    city: 'Riverside',
+    state: 'Califorina',
+    zipCode: '92510',
     residentCount: 5,
     bedCount: 5,
     counselorCount: 5,
-    director_name: "Tom Holland",
-    email: "tholland@riverside_rehab.org",
+    director_name: 'Tom Holland',
+    email: 'tholland@riverside_rehab.org',
     phone: 5126654350,
-    password: "ThisIsAPassword",
+    password: "passwordQ1",
   },
+
 ];
 
 const centerSeed = () => Center.bulkCreate(centerData);
--- seeds/index.js
+++ seeds/index.js
@@ -4,21 +4,13 @@ const residentSeed = require("./residentData");
 const centerSeed = require("./centerData");
 
 const seedAll = async () => {
-<<<<<<< HEAD
-    await sequelize.sync({ force: true });
-=======
   await sequelize.sync({ force: true });
->>>>>>> cf6d728df6f40967540014f054a09692dbc87620
 
   await centerSeed();
 
   await counselorSeed();
 
-<<<<<<< HEAD
-    // await centerSeed();
-=======
   await residentSeed();
->>>>>>> cf6d728df6f40967540014f054a09692dbc87620
 
   process.exit(0);
 };
