aa7b6eef60b360c5c9a39252210bc39e93371524
Daniel Moore <wron2@hotmail.com>
2021-07-17 14:07:56 -0700

    Updated seed

--- seeds/eventData.js
+++ seeds/eventData.js
@@ -12,14 +12,40 @@ const eventData = [
     title: "Our Event",
     start: "7/20/2021",
     end: "7/21/2021",
-    center_id: 1,
+    center_id: 2,
   },
 
   {
     title: "We All Have An Event",
     start: "7/5/2021",
     end: "7/10/2021",
-    center_id: 1,
+    center_id: 3,
+  },
+  {
+    title: "Our Event",
+    start: "7/20/2021",
+    end: "7/21/2021",
+    center_id: 4,
+  },
+
+  {
+    title: "We All Have An Event",
+    start: "7/5/2021",
+    end: "7/10/2021",
+    center_id: 5,
+  },
+  {
+    title: "Our Event",
+    start: "7/20/2021",
+    end: "7/21/2021",
+    center_id: 6,
+  },
+
+  {
+    title: "We All Have An Event",
+    start: "7/5/2021",
+    end: "7/10/2021",
+    center_id: 3,
   },
 ];
 
@@ -30,14 +56,53 @@ const selfEventData = [
     title: "My Event",
     start: "7/17/2021",
     end: "7/18/2021",
-    center_id: 1,
+    counselor_id: 2,
   },
 
   {
     title: "My Other Event",
     start: "7/20/2021",
     end: "7/21/2021",
-    center_id: 1,
+    counselor_id: 2,
+  },
+  {
+    title: "My Event",
+    start: "7/17/2021",
+    end: "7/18/2021",
+    counselor_id: 1,
+  },
+
+  {
+    title: "My Other Event",
+    start: "7/20/2021",
+    end: "7/21/2021",
+    counselor_id: 1,
+  },
+  {
+    title: "My Event",
+    start: "7/17/2021",
+    end: "7/18/2021",
+    counselor_id: 3,
+  },
+
+  {
+    title: "My Other Event",
+    start: "7/20/2021",
+    end: "7/21/2021",
+    counselor_id: 3,
+  },
+  {
+    title: "My Event",
+    start: "7/17/2021",
+    end: "7/18/2021",
+    counselor_id: 4,
+  },
+
+  {
+    title: "My Other Event",
+    start: "7/20/2021",
+    end: "7/21/2021",
+    counselor_id: 5,
   },
 ];
 
